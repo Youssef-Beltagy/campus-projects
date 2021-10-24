c64513c97812b0f7c8dd483079348704df249b5d
molochxte <molochitis@gmail.com>
2021-10-24 10:33:49 +0000

    updated with django forms + used crispy-forms

--- homepage/urls.py
+++ homepage/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('projects/', views.allprojects, name='allprojects'),
     path('submit/', views.createproject, name='createproject'),
-    path('thanks/', views.postsubmit, name='postsubmit')
+    path('thanks/', views.postsubmit, name='postsubmit'),
+    path('your-project/', views.get_project_info, name='your-project')
 ]
